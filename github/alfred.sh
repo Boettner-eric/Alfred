a4e6e91bbf866922bb4b6bf5ebab29f50be8b6e6
boettner-eric <boettner.eric@gmail.com>
2025-08-18 10:53:12 -0700

    fix: wrong sign

--- github/alfred.sh
+++ github/alfred.sh
@@ -1,6 +1,6 @@
 if test -f "repos.json"; then
     jq -f alfred.jq repos.json
-    if [ $(jq -r 'now - (.cache_time | tonumber) < 300' repos.json) = "true" ]; then
+    if [ $(jq -r 'now - (.cache_time | tonumber) > 300' repos.json) = "true" ]; then
         curl -s --user "$1:$2" https://api.github.com/user/repos | jq ". | {items: ., cache_time: now}" > repos.json &
     fi
 else
