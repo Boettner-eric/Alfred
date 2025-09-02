07c6f5557f490205e9d49cd75871f5d87ac4bdf4
boettner-eric <boettner.eric@gmail.com>
2025-09-02 14:13:48 -0700

    fix: call github script with env vars

--- github/alfred.sh
+++ github/alfred.sh
@@ -1,9 +1,9 @@
 if test -f "repos.json"; then
     jq -f alfred.jq repos.json
     if [ $(jq -r 'now - (.cache_time | tonumber) > 300' repos.json) = "true" ]; then
-        curl -s --user "$1:$2" https://api.github.com/user/repos?per_page=100 | jq ". | {items: ., cache_time: now}" > repos.json &
+        curl -s --user "$Username:$Token" https://api.github.com/user/repos?per_page=100 | jq ". | {items: ., cache_time: now}" > repos.json &
     fi
 else
-    curl -s --user "$1:$2" https://api.github.com/user/repos?per_page=100 | jq ". | {items: ., cache_time: now}" > repos.json
+    curl -s --user "$Username:$Token" https://api.github.com/user/repos?per_page=100 | jq ". | {items: ., cache_time: now}" > repos.json
     jq -f alfred.jq repos.json
 fi
